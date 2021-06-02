561bef5635ffe09057b057bc52f3e6c6e6ca0459
Marius Paavel <marius.paavel@gmail.com>
2021-06-02 15:39:18 +0000

    fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,8 +25,8 @@ export class AppComponent {
 		return Math.round(this.wpm).toString();
 	}	
 	
-	correct = new Audio("../assets/correct.wav");
-	wrong = new Audio("../assets/wrong.wav");
+	correct = new Audio("/keyboardwarrior/assets/correct.wav");
+	wrong = new Audio("/keyboardwarrior/assets/wrong.wav");
 
 	constructor() {	
 	}
